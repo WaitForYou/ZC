c3dc1e3b196384e885ca30956fb37b236deda4e7
jiumogaoao <394127821@qq.com>
2015-05-19 08:15:13 +0800

    change
    change

--- zhongchou/js/view.js
+++ zhongchou/js/view.js
@@ -237,12 +237,31 @@ app.views.index = Backbone.View.extend({
 */
 $('<div class="index_top">'
 	+'<div class="index_top_left">'
-
+		+'<div class="index_message_head">全国首家专业房地产众筹平台</div>'
+		+'<div class="index_message_frame">'
+			+'<div class="index_message_list">资金全程托管 安全可靠</div>'
+			+'<div class="index_message_list">投资门槛低 100元即可参与</div>'
+			+'<div class="index_message_list">专业、优秀的风控及资管团队</div>'
+			+'<div class="index_message_list">一线城市或地标优质资产</div>'
+			+'<div class="index_message_list">注册即可享受随机红包10-100元</div>'
+		+'</div>'
+		+'<div class="index_message_foot"></div>'
 	+'</div>'
 	+'<div class="index_top_right">'
-		+'<div class="index_canvar_1"></div>'
-		+'<div class="index_canvar_2"></div>'
-		+'<div class="index_canvar_3"></div>'
+		+'<div class="index_canvar_1">'
+			+'<div class="index_canvar_title">图1</div>'
+			+'<canvas id="canvar_1" class="index_canvar_left" width="125" height="125"></canvas>'
+			+'<div class="index_canvar_right"></div>'
+			+'<div class="clear"></div>'
+		+'</div>'
+		+'<div class="index_canvar_2">'
+			+'<div class="index_canvar_title">图2</div>'
+			+'<canvas id="canvar_2" class="index_canvar_single"></canvas>'
+		+'</div>'
+		+'<div class="index_canvar_3">'
+			+'<div class="index_canvar_title">图3</div>'
+			+'<canvas id="canvar_3" class="index_canvar_single"/></canvas>'
+		+'</div>'
 		+'<div class="index_promo_1"></div>'
 		+'<div class="index_promo_2"></div>'
 		+'<div class="index_promo_3"></div>'
@@ -279,6 +298,105 @@ $('<div class="index_top">'
 		          demo.onmouseout=function() {MyMar=setInterval(Marquee,speed);};  
               }
        */
+       var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
+		var lineChartData = {
+			labels : ["January","February","March","April","May","June","July"],
+			datasets : [
+				{
+					label: "My First dataset",
+					fillColor : "rgba(220,220,220,0.2)",
+					strokeColor : "rgba(220,220,220,1)",
+					pointColor : "rgba(220,220,220,1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(220,220,220,1)",
+					data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+				},
+				{
+					label: "My Second dataset",
+					fillColor : "rgba(151,187,205,0.2)",
+					strokeColor : "rgba(151,187,205,1)",
+					pointColor : "rgba(151,187,205,1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(151,187,205,1)",
+					data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+				}
+			]
+
+		}
+
+	var ctx = document.getElementById("canvar_3").getContext("2d");
+		window.myLine = new Chart(ctx).Line(lineChartData, {
+			responsive: true
+		})
+
+var pieData = [
+				{
+					value: 300,
+					color:"#F7464A",
+					highlight: "#FF5A5E",
+					label: "Red"
+				},
+				{
+					value: 50,
+					color: "#46BFBD",
+					highlight: "#5AD3D1",
+					label: "Green"
+				},
+				{
+					value: 100,
+					color: "#FDB45C",
+					highlight: "#FFC870",
+					label: "Yellow"
+				},
+				{
+					value: 40,
+					color: "#949FB1",
+					highlight: "#A8B3C5",
+					label: "Grey"
+				},
+				{
+					value: 120,
+					color: "#4D5360",
+					highlight: "#616774",
+					label: "Dark Grey"
+				}
+
+			];
+
+			var ctx1 = document.getElementById("canvar_1").getContext("2d");
+				window.myPie = new Chart(ctx1).Pie(pieData);
+				$("#canvar_1").width(160);
+				$("#canvar_1").height(125);
+	var radarChartData = {
+		labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+		datasets: [
+			{
+				label: "My First dataset",
+				fillColor: "rgba(220,220,220,0.2)",
+				strokeColor: "rgba(220,220,220,1)",
+				pointColor: "rgba(220,220,220,1)",
+				pointStrokeColor: "#fff",
+				pointHighlightFill: "#fff",
+				pointHighlightStroke: "rgba(220,220,220,1)",
+				data: [65,59,90,81,56,55,40]
+			},
+			{
+				label: "My Second dataset",
+				fillColor: "rgba(151,187,205,0.2)",
+				strokeColor: "rgba(151,187,205,1)",
+				pointColor: "rgba(151,187,205,1)",
+				pointStrokeColor: "#fff",
+				pointHighlightFill: "#fff",
+				pointHighlightStroke: "rgba(151,187,205,1)",
+				data: [28,48,40,19,96,27,100]
+			}
+		]
+	};
+	window.myRadar = new Chart(document.getElementById("canvar_2").getContext("2d")).Radar(radarChartData, {
+			responsive: true
+		});
 		console.log(this.data);//http://mini.114dianxin.com/pop2/images/bg_阴.png
 		var that = this;
 		//公告
