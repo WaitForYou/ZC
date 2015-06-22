100e5deeafddd0c5b7ea5a54ae0d1f654464c978
jiumogaoao <394127821@qq.com>
2015-06-23 07:00:01 +0800

    grunt
    grunt

---
+++ zhongchou/Gruntfile.js
@@ -0,0 +1,53 @@
+// JavaScript Document
+module.exports = function(grunt){
+	'use strict';
+    grunt.initConfig({
+        concat: {
+            options: {                                       //配置
+                stripBanners:true,
+                banner: '/*! This is the grunt test ' +      //添加自定义的banner
+                '<%= grunt.template.today("yyyy-mm-dd") %> */'
+            },
+            basic: {                                         //另一个任务
+                files: {                                     //另一种更简便的写法
+                    'dist/css/style.css': ['css/*.css']
+                }
+            },
+			ui: {                                         //另一个任务
+                files: {                                     //另一种更简便的写法
+                    'dist/css/base/ui.css': ['css/base/*.css']
+                }
+            }
+        },
+
+        uglify: {
+            options: {
+                banner: '/*! This is uglify test - ' +
+                '<%= grunt.template.today("yyyy-mm-dd") %> */'
+            },
+            common: {
+                files: {
+                    'dist/js/common.js': ['js/common.js','js/router.js','js/view.js','js/api.js','js/fn.js','js/obj.js']
+                }
+            },
+			lib: {
+                files: {
+                    'dist/js/lib.js': ['js/jquery-1.11.2.min.js','js/config.js','js/cookies.min.js','js/idcode/jquery.idcode.js','js/jquery.qrcode.js','js/qrcode.js','js/jquery-form.js','js/underscore-min.js','js/extend/slides.min.jquery.js','js/ui/core.js','js/ui/widget.js','js/ui/position.js','js/ui/menu.js','js/ui/button.js','js/ui/spinner.js','js/ui/selectmenu.js','js/ui/mouse.js','js/ui/slider.js','js/ui/datepicker.js','js/ui/datepicker-zh-CN.js','ueditor/ueditor.config.js','ueditor/ueditor.all.min.js','js/require.js','js/echarts.js']
+                }
+            }
+        },
+
+        watch: {
+            another: {
+                files: ['js/*.js'],
+                tasks: ['uglify']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['concat','uglify']);
+}
--- zhongchou/js/common.js
+++ zhongchou/js/common.js
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -136,30 +119,5 @@ app.views={};
 app.objs={};
 app.fns={};
 $.support.cors = true;
-//$(".test").singleTap(function(){
-//	alert("单击")
-//	})
-//$(".test").longTap(function(){
-//	alert("长按")
-//	})
 
 
-//args={
-//    iniL:30,//X方向滑动的最小距离
-//    iniT:50,//Y方向滑动的最大距离
-//    eCallback:function(tPoint){
-//        switch(tPoint.direction){
-//            case "left":
-//                alert("mx "+tPoint.mX+" my "+tPoint.mY+" left");
-//                break;
-//            case "right":
-//                alert("mx "+tPoint.mX+" my "+tPoint.mY+" right");
-//        }
-//    },
-//    longCallback:function(tPoint){
-//        alert("长按")
-//    }
-//}
-//$("body").longTap(function(){alert("长按")});
-//$("body").doubleTap(function(){alert("双击")})
-//$("body").Swipe(args); 
